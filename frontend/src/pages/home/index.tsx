1c5788d16d66e834a7b7e96351c812ddfd41c485
Hexu <bogdantomek987@gmail.com>
2024-06-26 21:53:42 +0200

    Spring boot, node.js

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,3 @@
+export default function() {
+    return <h1 className="font=bold underline"> Hello from Home, Next.js</h1>
+}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1 className="text-3x1 font-bold underline">Hello, Next.js!</h1>
+  }
